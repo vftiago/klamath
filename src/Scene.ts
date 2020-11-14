00563547d828723fa5961a282a780e69f2caaf8e
root <vf.tiago@gmail.com>
2020-11-14 16:28:00 +0000

    delete bloat

--- src/Scene.ts
+++ src/Scene.ts
@@ -30,41 +30,32 @@ const Scene = (texture: THREE.Texture) => {
   const createAsteroidField = function() {
     const geometry_base = new THREE.SphereBufferGeometry(1.2, 6, 4);
     const attr = geometry_base.attributes;
-    const geometry = new THREE.BufferGeometry();
+    const geometry = new THREE.SphereBufferGeometry(88);
     const vertices_base = [];
     const radiuses_base = [];
     const radians_base = [];
     const scales_base = [];
     const indices_base: number[] = [];
     for (let i = 0; i < ASTEROID_COUNT; i ++) {
-      const radius = Util.getRandomInt(300, 1000);
-      const radian = Util.getRadian(Util.getRandomInt(0, 3600) / 10);
-      const scale = Util.getRandomInt(60, 120) / 100;
+      const fieldRadius = Util.getRandomInt(300, 1000);
+      const fieldRadian = Util.getRadian(Util.getRandomInt(0, 3600) / 10);
+      const asteriodScale = Util.getRandomInt(120, 120) / 100;
       for (let j = 0; j < attr.position.array.length; j += 3) {
         vertices_base.push(
           attr.position.array[j + 0],
           attr.position.array[j + 1],
           attr.position.array[j + 2]
         );
-        radiuses_base.push(radius);
-        radians_base.push(radian);
-        scales_base.push(scale);
+        radiuses_base.push(fieldRadius);
+        radians_base.push(fieldRadian);
+        scales_base.push(asteriodScale);
       }
 
       Object.values(geometry_base!.index!.array).map((item: number) => {
         return indices_base.push(item + i * attr.position.array.length / 3)
       });
     }
-    const vertices = new Float32Array(vertices_base);
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    const radius = new Float32Array(radiuses_base);
-    geometry.setAttribute('radius', new THREE.BufferAttribute(radius, 1));
-    const radians = new Float32Array(radians_base);
-    geometry.setAttribute('radian', new THREE.BufferAttribute(radians, 1));
-    const scales = new Float32Array(scales_base);
-    geometry.setAttribute('scale', new THREE.BufferAttribute(scales, 1));
-    const indices = new Uint32Array(indices_base);
-    geometry.setIndex(new THREE.BufferAttribute(indices, 1));
+
     const material = new THREE.ShaderMaterial({
       uniforms: THREE.UniformsUtils.merge([
         THREE.UniformsLib['lights'],
@@ -75,10 +66,10 @@ const Scene = (texture: THREE.Texture) => {
           },
         }
       ]),
-      vertexShader: require('./glsl/object.vert').default,
-      fragmentShader: require('./glsl/object.frag').default,
       lights: true,
+      wireframe: true
     });
+    
     return new THREE.Mesh(geometry, material);
   };
 
@@ -110,11 +101,6 @@ const Scene = (texture: THREE.Texture) => {
     logo = Logo(texture);
     logo.mesh.position.y = 133;
     logo.mesh.rotation.x = -0.4;
-    // logo.mesh.rotation.y = 1.5;
-
-
-    // logo.mesh.position.anchor.set(1, 1, 0);
-    // logo.mesh.rotation.set(0,1.5,0)
     scene.add(logo.mesh);
     background = createBackground();
     scene.add(background);
@@ -126,12 +112,8 @@ const Scene = (texture: THREE.Texture) => {
     scene.add(light);
 
     camera.force.position.anchor.set(0, 400, 1000);
-    // camera.force.look.anchor.set(0, 0, 0);
   }
 
-  //
-  // common process
-  //
   const resizeWindow = () => {
     canvas.width = document.body.clientWidth;
     canvas.height = window.innerHeight;
@@ -148,9 +130,6 @@ const Scene = (texture: THREE.Texture) => {
     camera.force.position.applyDrag(0.8);
     camera.force.position.updateVelocity();
     camera.updatePosition();
-    // camera.force.look.anchor.y = Math.sin(elapsedTime / 20) * 100;
-    // camera.force.look.applyHook(0, 0.2);
-    // camera.force.look.applyDrag(0.4);
     camera.updateLook();
     renderer.setRenderTarget(null);
     renderer.render(scene, camera);
@@ -168,7 +147,6 @@ const Scene = (texture: THREE.Texture) => {
   const init = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0xeeeeee, 0.0);
-    // camera.position.set(1000, 1000, 1000);
     camera.lookAt(new THREE.Vector3());
 
     on();
