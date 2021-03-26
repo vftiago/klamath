f7379844a2e6f9ce90d879379ce5e77af8e0e25c
tiago <vftiago@gmail.com>
2021-03-26 21:24:59 +0000

    format breakpoints.ts

--- src/common/breakpoints.ts
+++ src/common/breakpoints.ts
@@ -3,9 +3,11 @@ const size = {
   sm: '768px',
   lg: '1200px',
 };
+
 const device = {
   xs: `(min-width: ${size.xs})`,
   sm: `(min-width: ${size.sm})`,
   lg: `(min-width: ${size.lg})`,
 };
+
 export default { size, device };
