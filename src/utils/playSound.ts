0610bd46605355ae8a7fd755048bc3f790e49f2f
tiago <vftiago@gmail.com>
2021-04-04 16:50:28 +0100

    catch audio element errors

--- src/utils/playSound.ts
+++ src/utils/playSound.ts
@@ -1,10 +1,16 @@
 import { MutableRefObject } from "react";
 
-const playSound = (audioElement: MutableRefObject<HTMLAudioElement | null>) => {
+const playSound = async (
+  audioElement: MutableRefObject<HTMLAudioElement | null>,
+) => {
   if (audioElement.current != null) {
     audioElement.current.pause();
     audioElement.current.currentTime = 0;
-    audioElement.current.play();
+    try {
+      await audioElement.current.play();
+    } catch (e) {
+      console.error(e);
+    }
   }
 };
 
