a83fef58add231fecf9faca094e3f48cc000542f
tiago <vftiago@gmail.com>
2022-03-14 19:30:18 +0000

    remove cringe intro modal

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -3,7 +3,6 @@
 import { jsx } from "@emotion/core";
 import { Fragment, useEffect, useRef, useState } from "react";
 import App from "./App";
-import Prologue from "./Prologue";
 import Scene from "./scene/Scene";
 import buttonClick from "../assets/audio/button-click.mp3";
 import buttonHover from "../assets/audio/button-hover.mp3";
@@ -11,72 +10,54 @@ import playSound from "../utils/playSound";
 import Footer from "./Footer";
 
 function AppContainer() {
-  const [ready, setReady] = useState(false);
-  const [muted, setMuted] = useState(true);
+	const [muted, setMuted] = useState(true);
 
-  const buttonClickAudioElement = useRef(null);
-  const buttonHoverAudioElement = useRef(null);
+	const buttonClickAudioElement = useRef(null);
+	const buttonHoverAudioElement = useRef(null);
 
-  const handleVisibilityChange = () => {
-    if (muted) return;
-    document.hidden ? setMuted(true) : setMuted(false);
-  };
+	const handleVisibilityChange = () => {
+		if (muted) return;
+		document.hidden ? setMuted(true) : setMuted(false);
+	};
 
-  useEffect(() => {
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-    return () => {
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-    };
-  }, []);
+	useEffect(() => {
+		document.addEventListener("visibilitychange", handleVisibilityChange);
+		return () => {
+			document.removeEventListener("visibilitychange", handleVisibilityChange);
+		};
+	}, [handleVisibilityChange]);
 
-  const onHeadphonesIconClick = () => {
-    setMuted(!muted);
-  };
+	const onHeadphonesIconClick = () => {
+		setMuted(!muted);
+	};
 
-  return (
-    <Fragment>
-      <audio
-        src={buttonClick}
-        ref={buttonClickAudioElement}
-        muted={muted}
-      ></audio>
-      <audio
-        src={buttonHover}
-        ref={buttonHoverAudioElement}
-        muted={muted}
-      ></audio>
-      <Scene />
-      {ready ? (
-        <App
-          muted={muted}
-          onHeadphonesIconClick={onHeadphonesIconClick}
-          onButtonClick={() => {
-            playSound(buttonClickAudioElement);
-          }}
-          onButtonHover={() => {
-            playSound(buttonHoverAudioElement);
-          }}
-        />
-      ) : (
-        <Prologue
-          onModalVisible={() => {
-            setMuted(false);
-          }}
-          onReady={() => {
-            setMuted(false);
-            setReady(true);
-          }}
-          onButtonClick={() => {
-            playSound(buttonClickAudioElement);
-          }}
-          onButtonHover={() => {
-            playSound(buttonHoverAudioElement);
-          }}
-        />
-      )}
-      <Footer />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<audio
+				src={buttonClick}
+				ref={buttonClickAudioElement}
+				muted={muted}
+			></audio>
+			<audio
+				src={buttonHover}
+				ref={buttonHoverAudioElement}
+				muted={muted}
+			></audio>
+			<Scene />
+			<App
+				muted={muted}
+				onHeadphonesIconClick={onHeadphonesIconClick}
+				onButtonClick={() => {
+					playSound(buttonClickAudioElement);
+				}}
+				onButtonHover={() => {
+					playSound(buttonHoverAudioElement);
+				}}
+			/>
+
+			<Footer />
+		</Fragment>
+	);
 }
 
 export default AppContainer;
