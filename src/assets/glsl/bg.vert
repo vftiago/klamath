28b5dfdfc69ccb42c078aaada5040fd86d08f114
tiago <vftiago@gmail.com>
2022-06-03 20:42:39 +0100

    🔥 remove code used before the transition to react-three-fiber

--- src/assets/glsl/bg.vert
+++
@@ -1,8 +0,0 @@
-uniform float time;
-
-varying vec3 vColor;
-
-void main(){
-  vColor=vec3((position.y/920.+1.)*.12+.9);
-  gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1.);
-}
--- src/components/icons/LoadingIcon.tsx
+++ src/components/icons/LoadingIcon.tsx
@@ -2,107 +2,105 @@
 
 import React from "react";
 
-const LoadingIcon = ({ size = 16, duration = 1 }) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			style={{ margin: "auto" }}
-			width={size}
-			height={size}
-			display="block"
-			preserveAspectRatio="xMidYMid"
-			viewBox="0 0 100 100"
-		>
-			<path fill="#333" d="M0 0H20V20H0z">
-				<animate
-					attributeName="fill"
-					begin="0s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M40 0H60V20H40z">
-				<animate
-					attributeName="fill"
-					begin="0.125s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M80 0H100V20H80z">
-				<animate
-					attributeName="fill"
-					begin="0.25s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M0 40H20V60H0z">
-				<animate
-					attributeName="fill"
-					begin="0.875s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M80 40H100V60H80z">
-				<animate
-					attributeName="fill"
-					begin="0.375s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M0 80H20V100H0z">
-				<animate
-					attributeName="fill"
-					begin="0.75s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M40 80H60V100H40z">
-				<animate
-					attributeName="fill"
-					begin="0.625s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-			<path fill="#333" d="M80 80H100V100H80z">
-				<animate
-					attributeName="fill"
-					begin="0.5s"
-					calcMode="discrete"
-					dur={`${duration}s`}
-					keyTimes="0;0.125;1"
-					repeatCount="indefinite"
-					values="#888888;#333333;#333333"
-				></animate>
-			</path>
-		</svg>
-	);
-};
+const LoadingIcon = ({ size = 16, duration = 1 }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		style={{ margin: "auto" }}
+		width={size}
+		height={size}
+		display="block"
+		preserveAspectRatio="xMidYMid"
+		viewBox="0 0 100 100"
+	>
+		<path fill="#333" d="M0 0H20V20H0z">
+			<animate
+				attributeName="fill"
+				begin="0s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M40 0H60V20H40z">
+			<animate
+				attributeName="fill"
+				begin="0.125s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M80 0H100V20H80z">
+			<animate
+				attributeName="fill"
+				begin="0.25s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M0 40H20V60H0z">
+			<animate
+				attributeName="fill"
+				begin="0.875s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M80 40H100V60H80z">
+			<animate
+				attributeName="fill"
+				begin="0.375s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M0 80H20V100H0z">
+			<animate
+				attributeName="fill"
+				begin="0.75s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M40 80H60V100H40z">
+			<animate
+				attributeName="fill"
+				begin="0.625s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+		<path fill="#333" d="M80 80H100V100H80z">
+			<animate
+				attributeName="fill"
+				begin="0.5s"
+				calcMode="discrete"
+				dur={`${duration}s`}
+				keyTimes="0;0.125;1"
+				repeatCount="indefinite"
+				values="#888888;#333333;#333333"
+			></animate>
+		</path>
+	</svg>
+);
 
 export default LoadingIcon;
