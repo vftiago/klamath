89fa813c5acf62bb4c886c24fcb61af292d5677b
tiago <vftiago@gmail.com>
2023-02-08 20:30:30 +0000

    💄 update logo .svg

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,19 +1,50 @@
 import React from "react";
 
-const Icon = ({ size = 16 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    data-name="Layer 1"
-    viewBox="0 0 143.72 224.57"
-    width={size}
-    height={size}
-  >
-    <path
-      d="M993 652.61v-5-196.95c0-10.41 8.68-19.91 19.17-20.66 9.37-.67 18.77-1 28.5-1.5v32.28h-15.31v32h15.34v32H1025v5.59c0 31.66.1 63.32-.07 95 0 7.24-1.47 14.21-6.73 19.93-6.88 7.54-15.39 8.7-25.2 7.31zM897 429.22h31.85v31.56h15.83v32h-15.41V652.2c-15.66 3.43-27.28-3-31.5-15.93A14.19 14.19 0 01897 632q-.06-99.5 0-199z"
-      className="cls-1"
-      transform="translate(-897 -428.54)"
-    ></path>
-  </svg>
-);
+function Logo({ size = 16 }) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			x="0"
+			y="0"
+			enableBackground="new 0 0 136 192"
+			version="1.1"
+			viewBox="0 0 136 192"
+			xmlSpace="preserve"
+			width={size}
+			height={size}
+		>
+			<g>
+				<path
+					d="M32 0H0v171.4C0 182.8 10.7 192 24 192h8V0z"
+					className="st0"
+				></path>
+			</g>
+			<g>
+				<g>
+					<path d="M128 0H136V24H128z" className="st0"></path>
+					<path d="M136 0L128 0 128 24 136 24 136 0z" className="st0"></path>
+				</g>
+			</g>
+			<g>
+				<path
+					d="M128 0h-8c-13.3 0-24 9.2-24 20.6V192h8c13.3 0 24-9.2 24-20.6V0z"
+					className="st0"
+				></path>
+			</g>
+			<g>
+				<g>
+					<path d="M128 48H136V72H128z" className="st0"></path>
+					<path d="M136 48L128 48 128 72 136 72 136 48z" className="st0"></path>
+				</g>
+			</g>
+			<g>
+				<g>
+					<path d="M32 24H40V48H32z" className="st0"></path>
+					<path d="M40 24L32 24 32 48 40 48 40 24z" className="st0"></path>
+				</g>
+			</g>
+		</svg>
+	);
+}
 
-export default Icon;
+export default Logo;
