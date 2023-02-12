3a26be7128cf322c3da936721eaac4bdf36c7a31
tiago <vftiago@gmail.com>
2023-02-12 21:32:42 +0000

    🍱 update logo

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -4,43 +4,55 @@ function Logo({ size = 16 }) {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			x="0"
-			y="0"
-			enableBackground="new 0 0 136 192"
-			version="1.1"
-			viewBox="0 0 136 192"
-			xmlSpace="preserve"
-			width={size}
+			viewBox="0 0 102 144"
 			height={size}
+			width={size}
 		>
-			<g>
-				<path
-					d="M32 0H0v171.4C0 182.8 10.7 192 24 192h8V0z"
-					className="st0"
-				></path>
-			</g>
-			<g>
-				<g>
-					<path d="M128 0H136V24H128z" className="st0"></path>
-					<path d="M136 0L128 0 128 24 136 24 136 0z" className="st0"></path>
+			<g data-name="Layer 2">
+				<g className="cls-1" data-name="Rectangle 2">
+					<g data-name="Rectangle 2">
+						<path d="M0 0v126a18 18 0 0018 18h6V0z" className="cls-2"></path>
+						<path d="M24 0H0v126a18 18 0 0018 18h6V0z" className="cls-2"></path>
+					</g>
 				</g>
-			</g>
-			<g>
-				<path
-					d="M128 0h-8c-13.3 0-24 9.2-24 20.6V192h8c13.3 0 24-9.2 24-20.6V0z"
-					className="st0"
-				></path>
-			</g>
-			<g>
-				<g>
-					<path d="M128 48H136V72H128z" className="st0"></path>
-					<path d="M136 48L128 48 128 72 136 72 136 48z" className="st0"></path>
+				<g className="cls-1" data-name="Rectangle 2">
+					<g data-name="Rectangle 2">
+						<path d="M96 0H102V24H96z" className="cls-2"></path>
+						<path
+							d="M102 0L96 0 96 24 102 24 102 0 102 0z"
+							className="cls-2"
+						></path>
+					</g>
 				</g>
-			</g>
-			<g>
-				<g>
-					<path d="M32 24H40V48H32z" className="st0"></path>
-					<path d="M40 24L32 24 32 48 40 48 40 24z" className="st0"></path>
+				<g className="cls-1" data-name="Rectangle 2">
+					<g data-name="Rectangle 2">
+						<path d="M24 24H30V48H24z" className="cls-2"></path>
+						<path
+							d="M30 24L24 24 24 48 30 48 30 24 30 24z"
+							className="cls-2"
+						></path>
+					</g>
+				</g>
+				<g className="cls-1" data-name="Rectangle 2">
+					<g data-name="Rectangle 2">
+						<path
+							d="M90 0a18 18 0 00-18 18v126h6a18 18 0 0018-18V0z"
+							className="cls-2"
+						></path>
+						<path
+							d="M96 0h-6a18 18 0 00-18 18v126h6a18 18 0 0018-18V0z"
+							className="cls-2"
+						></path>
+					</g>
+				</g>
+				<g className="cls-1" data-name="Rectangle 2">
+					<g data-name="Rectangle 2">
+						<path d="M96 48H102V72H96z" className="cls-2"></path>
+						<path
+							d="M102 48L96 48 96 72 102 72 102 48 102 48z"
+							className="cls-2"
+						></path>
+					</g>
 				</g>
 			</g>
 		</svg>
