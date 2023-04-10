320501a0f29b12f461db4bdd2eb95f01610902f5
vftiago <vf.tiago@gmail.com>
2023-04-10 21:53:47 +0100

    🔥 delete unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import AppContainer from "./components/AppContainer";
 
 const root = document.getElementById("root");
@@ -11,8 +10,3 @@ createRoot(root!).render(
 		<AppContainer />
 	</React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
