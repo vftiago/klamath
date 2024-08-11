39a08430ef05a8dc4084879df6341eb68f0d7eef
vftiago <vf.tiago@gmail.com>
2024-08-11 17:52:14 +0100

    💄 simplify AnimatedHeadphonesIcon styles

--- src/components/icons/AnimatedHeadphonesIcon.tsx
+++ src/components/icons/AnimatedHeadphonesIcon.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import HeadphonesIcon from "./Headphones";
+import clsx from "clsx";
 
 const AnimatedHeadphonesIcon = ({ active }: { active: boolean }) => {
   return (
-    <div className="flex h-20 w-20 items-center justify-center transition duration-500 ease-out-expo">
-      <span className="flex items-center justify-center [&>span]:hover:bg-orange-600 [&>svg]:hover:fill-orange-600 ">
-        <HeadphonesIcon customStyles="fill-neutral-800transition duration-500 ease-out-expo" />
+    <div className="flex items-center justify-center transition duration-500 ease-out-expo">
+      <div className="relative flex items-center justify-center [&>span]:hover:bg-orange-600 [&>svg]:hover:fill-orange-600">
+        <HeadphonesIcon customStyles="fill-neutral-800 transition duration-500 ease-out-expo" />
         <span
-          className={`absolute left-1/2 -ml-[13px] h-[2px] w-7 -rotate-45 scale-x-0 bg-black transition duration-500 ease-out-expo ${
-            active ? "" : "-rotate-45 scale-x-100"
-          }`}
+          className={clsx(
+            `absolute left-1/2 top-0 -ml-3 mt-2 h-[2px] w-6 -rotate-45 scale-x-0 bg-black transition duration-500 ease-out-expo`,
+            active ? "" : "scale-x-100",
+          )}
         ></span>
-      </span>
+      </div>
     </div>
   );
 };
